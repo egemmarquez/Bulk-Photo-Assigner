bd989bbbe1e2d974c673ad937a3d84b77c0eb9c7
Edgar Marquez <52714942+egempiu@users.noreply.github.com>
2021-12-22 11:51:07 -0600

    TEST
    TEST

--- bulk_photoassigner.php
+++ bulk_photoassigner.php
@@ -1,4 +1,5 @@
 <?php
+echo "MERGE MAIN BRANCH TO QWE";
 //custom wordpress importer script
 //The script objective is to be able to import a database from a custom CMS to wordpress.
 //This script imports all the pictures from the old media folder of the Custom CMS (the photos are not actually registered as pictures on the wordpress database)  to be displayed as an external url Picture.
@@ -7,18 +8,18 @@
 //Config
 
 //WP database we are manipulating posts.
-$server = '';
-$username = '';
-$password = '';
-$database = '';
+$server = 'AA';
+$username = 'BB';
+$password = 'CC';
+$database = 'DD';
 $meta_id = '';
 $media_url = ''; // Media Folder Location
 //Behaivor.
 
 $amount = '2000'; //Amount of posts to be imported per iteration. You can configure the amount of posts depending on what your server can handle.
-$from = '0'; //Starting Amount. 
+$from = '0'; //Starting Amount.
 
-//1st step - Insert all the data from the we have on postmeta to the wp_posts table. We will use the wp_postmeta ID and we make sure to select the 
+//1st step - Insert all the data from the we have on postmeta to the wp_posts table. We will use the wp_postmeta ID and we make sure to select the
 
 $query = "SELECT * from wp_postmeta where meta_key = '".$meta_id."' order by meta_id desc limit $from, $amount";
 
@@ -36,7 +37,7 @@ while($row=$result->fetch_assoc())
 {
 if ($row['meta_value'] == "")
 {
-//jump result and dont insert anything	
+//jump result and dont insert anything
 }
 else {
 $insert = "insert into wp_postmeta (post_id, meta_key, meta_value) VALUES ($row[post_id], '_knawatfibu_url', 'https://$media_url/$row[meta_value]')";
